46442303ecf7c757dee6ccfe0d5f2e3d1c738d87
senfox <vos_sen@mail.ru>
2021-02-15 19:24:40 +0300

    New Remove console log2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let getRandom = (startNumber, endNumber) =>  Math.floor(Math.random() * (endNumber - startNumber) + startNumber);
+
+getRandom(5, 19);
+
 /*console.log(getRandom(0, 42));*/
 
 
@@ -11,6 +14,7 @@ let getRandom = (startNumber, endNumber) =>  Math.floor(Math.random() * (endNumb
 // причем числа функций только положительные. Дробные, само собой.
 
 let getRandomize = (startNumber, endNumber) =>  Math.random() * (endNumber - startNumber) + startNumber;
+getRandomize(0, 76.8);
 /*console.log(getRandomize(1, 42));*/
 
 // так выглядит стрелочность: const getWizardX = (gameFieldWidth) => (gameFieldWidth - WIZARD_WIDTH) / 2;
