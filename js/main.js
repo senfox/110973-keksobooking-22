d0129d5262cd9da1d8991d9798908622e4bb946b
senfox <vos_sen@mail.ru>
2021-02-16 09:35:58 +0300

    Final randomize

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-let getRandom = (startNumber, endNumber) =>  Math.floor(Math.random() * (endNumber - startNumber) + startNumber);
-
+const getRandom = (startNumber, endNumber) =>  Math.floor(Math.random() * (endNumber - startNumber) + startNumber);
 getRandom(5, 19);
 
-/*console.log(getRandom(0, 42));*/
-
-
-/*let randomizeGet = Math.random() * (endNumber - startNumber + 1) + startNumber;*/
-
-
-
-
-// функция делает то же, что предыдущая, - но возвращает не только число, но и число знаков после запятой.
-// причем числа функций только положительные. Дробные, само собой.
-
-let getRandomize = (startNumber, endNumber) =>  Math.random() * (endNumber - startNumber) + startNumber;
-getRandomize(0, 76.8);
-/*console.log(getRandomize(1, 42));*/
-
-// так выглядит стрелочность: const getWizardX = (gameFieldWidth) => (gameFieldWidth - WIZARD_WIDTH) / 2;
-
-
+const getRandomize = (startNumber, endNumber, dotSign) =>  (Math.random() * (endNumber - startNumber) + startNumber).toFixed(dotSign);
+getRandomize(0, 76.8, 4);
